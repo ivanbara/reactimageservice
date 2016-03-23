1f12553b5d83b2d82768fa980b90f3626f61c5f9
ivanbara <frostfiresoft@gmail.com>
2016-03-23 02:17:41 +0200

    comment components

---
+++ components/CommentComponent.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class CommentComponent extends React.Component{
+
+	constructor(props){
+		super(props);
+		this.state = {comment: ''}
+	}
+
+	render(){
+		Return (
+          <div>
+						<div className='comment'>
+							<img src='' className='commentAvatar'/>
+							<div className='commentOutput'>{this.props.comment}</div>
+						</div>
+          </div>
+      );
+	}
+
+}
+
+
+
+
+export default CommentComponent;
---
+++ components/CommentInputComponent.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class CommentInputComponent extends React.Component{
+
+	constructor(props){
+		super(props);
+		this.state = {inputField: ''}
+	}
+
+
+	render(){
+		return (
+          <div>
+						<div className='commentInput'>
+							<img src='' className='commentAvatar'/>
+							<div className='commentInputField'></div>
+						</div>
+          </div>
+      );
+	}
+}
+
+
+
+
+export default CommentInputComponent;
